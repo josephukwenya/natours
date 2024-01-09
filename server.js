48826a3ec8413ab525da544721450cc8e9556e6c
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2024-01-09 10:11:02 -0800

    mongodb remote connection

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app');
 
-const port = 5000;
+dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connected succesfully!'));
+
+const port = 5000;
 app.listen(port, () => {
   console.log(`App running on ${port}...`);
 });
